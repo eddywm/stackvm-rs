5cd2b1be5707382c84540f6e62d40c7da8065bed
Eddy WM <eddywmdev@gmail.com>
2020-06-14 14:00:39 +0200

    Make opcode top level short ints

--- src/opcode.rs
+++ src/opcode.rs
@@ -1,5 +1,3 @@
-enum OpCode {
-    IADD, // Integer addition opcode
-    ISUB, // Integer addition opcode
-    IMULT, // Integer addition opcode
-}
+const IADD: i8 = 1; // Integer addition opcode
+const ISUB: i8 = 2; // Integer addition opcode
+const IMULT: i8 = 3; // Integer addition opcode
