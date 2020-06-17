4fc185df9c1abade25b99c7278a2b41227af235f
Eddy WM <eddywmdev@gmail.com>
2020-06-17 14:27:16 +0200

    Add more op-codes

--- src/opcode.rs
+++ src/opcode.rs
@@ -13,4 +13,14 @@ enum OpCode {
     BR =    6,
     BRT =   7,
     BRF =   8,
+
+    ICONST = 9,   // Push constant integer
+    LOAD   = 10,  // Load from local context
+    GLOAD  = 11,  // Load from global memory
+    STORE  = 12,  // Store in local context
+    GSTORE = 13,  // Store in global memory
+    PRINT  = 14,  // Print stack top
+    POP  =   15,  // Throw away top of stack
+    CALL =   16,
+    RET  =   17,  // Return with/without value
 }
