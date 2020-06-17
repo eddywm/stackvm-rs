528b75e278693074dc5fb715dbc047ab4375d53d
Eddy WM <eddywmdev@gmail.com>
2020-06-17 12:54:56 +0200

    Move to enum - Add comparison and branching opcodes

--- src/opcode.rs
+++ src/opcode.rs
@@ -1,3 +1,16 @@
-const IADD: i8 = 1; // Integer addition opcode
-const ISUB: i8 = 2; // Integer addition opcode
-const IMULT: i8 = 3; // Integer addition opcode
+#[derive(Debug, Enum)]
+enum OpCode {
+    // Integers ops (Addition, Subtraction & Multiplication)
+    IADD =  1,
+    ISUB =  2,
+    IMULT = 3,
+
+    // Comparison (Less Than & Less Than)
+    ILET =  4,
+    IEQ =   5,
+
+    // Branching (Branch, Branch if true & if false)
+    BR =    6,
+    BRT =   7,
+    BRF =   8,
+}
